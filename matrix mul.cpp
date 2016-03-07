8af37adf3edf6c9c94d273e883570c503b1be6e5
Amir Sajjad Poursoltani <sajjadpoores@yahoo.com>
2016-03-07 22:15:53 +0330

    Update matrix mul.cpp

--- matrix mul.cpp
+++ matrix mul.cpp
@@ -6,9 +6,9 @@ bool matrix_mult(int **a,int ar,int ac, int **b,int br,int bc,int **c){
 	for (int i = 0; i < ar;i++)
 	for (int j = 0; j < bc; j++){
 		c[i][j] = 0;
-		for (int k = 0; k <ac ; k++){
+		for (int k = 0; k <ac ; k++)
 			c[i][j] += a[i][k] * b[k][j];
-		}
+		
 	}
 
 	return 1;
